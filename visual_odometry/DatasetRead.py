7db70606378790ce35b750608d45e055a33d1f0c
DizkiBorsuk <maciekrostocki@gmail.com>
2023-04-07 14:08:37 +0200

    bol dupy

--- visual_odometry/DatasetRead.py
+++ visual_odometry/DatasetRead.py
@@ -8,14 +8,14 @@ import os
 class ImportKittyDataset(): 
     def __init__(self, img_sequance = '00'):
         
-        self.sequance_dir = 'KITTY_dataset/sequences/{}/'.format(img_sequance) # directory of images
-        self.ground_truth_poses_dir = 'KITTY_dataset/ground_truth_poses/{}.txt'.format(img_sequance) 
+        self.sequance_dir = '../KITTY_dataset/sequences/{}/'.format(img_sequance) # directory of images
+        self.ground_truth_poses_dir = '../KITTY_dataset/ground_truth_poses/{}.txt'.format(img_sequance) 
         
         self.L_camera_imgs_dir = sorted(os.listdir(self.sequance_dir + 'image_0'))
         self.R_camera_imgs_dir = sorted(os.listdir(self.sequance_dir + 'image_1'))
         self.imgs_num = len(self.L_camera_imgs_dir)
             
-        calib = pd.read_csv('KITTY_dataset/sequences/{}/calib.txt'.format(img_sequance), delimiter=' ',header=None, index_col=0)
+        calib = pd.read_csv('../KITTY_dataset/sequences/{}/calib.txt'.format(img_sequance), delimiter=' ',header=None, index_col=0)
         self.P0 = np.array(calib.loc['P0:']).reshape((3,4))
         self.P1 = np.array(calib.loc['P1:']).reshape((3,4)) # P0 and P1 are for grayscale images 
         self.P2 = np.array(calib.loc['P2:']).reshape((3,4)) # P2 and P3 are for RGB images
@@ -31,12 +31,13 @@ class ImportKittyDataset():
         
     def getGTposes(self): 
         ground_truth_poses = pd.read_csv(self.ground_truth_poses_dir, delimiter=' ', header=None) #read ground truth poses
-        gt =np.zeros((len(ground_truth_poses), 3, 4))
+        gt_poses =np.zeros((len(ground_truth_poses), 3, 4))
         
-        for i in ground_truth_poses: 
-            gt[i] = np.array(ground_truth_poses.iloc[i]).reshape((3,4))
+        for npose in range(len(ground_truth_poses)): 
+            gt_poses[npose] = (np.array(ground_truth_poses.iloc[npose]).reshape((3,4)))
         
-        return gt
+        print('poses shape', gt_poses.shape)
+        return gt_poses
         
         
     def readImgs(self, camera_type):
