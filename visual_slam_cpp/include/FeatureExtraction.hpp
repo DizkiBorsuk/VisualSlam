428151208281e136f026f6a3988dbcaf0dd6bf89
DizkiBorsuk <maciekrostocki@gmail.com>
2023-07-16 23:22:21 +0200

    change to frame format in cpp

--- visual_slam_cpp/include/FeatureExtraction.hpp
+++ visual_slam_cpp/include/FeatureExtraction.hpp
@@ -1,53 +1,62 @@
 #pragma once 
 #include "system.hpp"
-#include "monoSLAM.hpp"
 
 namespace mrVSLAM
 {
+    enum ExtractorType {featureMono, featureStereo, direct}; 
+    enum MatcherType {}; 
+
+
     class FeatureExtraction
     {
 
     public:  
         std::vector<cv::KeyPoint>  frame_keypoints; 
-        std::vector<cv::DMatch> good_matches; 
-        std::vector<std::vector<cv::Point2f>> matched_keypoints; 
+        cv::Mat descriptors; 
+
 
         FeatureExtraction() noexcept {} //default constructor 
-        // FeatureExtraction(const std::string &desType, const bool GPU, const int numberOfFeatures) noexcept; 
-        FeatureExtraction(const monoSLAM::ExtractorType, const bool GPU, const int numberOfFeatures) noexcept;
+        FeatureExtraction(const ExtractorType, const bool GPU, const int numberOfFeatures) noexcept;
 
         void getFeatures(const cv::Mat frame) noexcept; 
         void getGPUFeatures(const cv::cuda::GpuMat frame) noexcept;
-        void matchFeaturesFlann(const float& low_rt = 0.7f) noexcept; 
-        void matchFeaturesBF(const float& low_rt = 0.7f) noexcept;
+
         //void matchGPUFeaturesBF(const float& low_rt = 0.7f) noexcept;
     private:
-
+        // CPU detector and descriptor objects declaration 
         cv::Ptr<cv::FeatureDetector> detector;   // detector object declaration  
-        cv::Ptr<cv::DescriptorExtractor> descriptor; // descriptor object declaration 
-        
-        cv::Ptr<cv::cuda::ORB> gpu_orb_extractor;
-        cv::Ptr<cv::cuda::FastFeatureDetector> gpu_fast_detector; 
+        cv::Ptr<cv::DescriptorExtractor> descriptor; 
 
-        cv::cuda::GpuMat gpu_descriptors, gpu_prev_descriptors, gpu_keypoints, prev_gpu_keyPs;
-        cv::Mat descriptors, prev_descriptors;
+        cv::Ptr<cv::cuda::ORB> gpuOrbExtractor;
+        cv::Ptr<cv::cuda::FastFeatureDetector> gpuFastDetector; 
 
-        cv::Ptr<cv::DescriptorMatcher> matcher; // feature matcher object declaration 
-        std::vector<cv::KeyPoint> prev_keyPs;
-        
-        cv::Ptr<cv::cuda::DescriptorMatcher> gpu_matcher; 
-
-
-        typedef std::vector<uint32_t> descType;
-        //void computeORB(const cv::Mat &frame, std::vector<cv::KeyPoint> &keyPoints, std::vector<descType> &descriptors); 
     }; 
+
+    class FrameMatcher
+    {
+    public: 
+        std::vector<cv::DMatch> good_matches; 
+        std::vector<std::vector<cv::Point2f>> matched_keypoints; 
+        void matchFeaturesFlann(const float& low_rt = 0.7f) noexcept; 
+        void matchFeaturesBF(const float& low_rt = 0.7f) noexcept;
+
+    private:
+        cv::Ptr<cv::DescriptorMatcher> matcher;
+        cv::Ptr<cv::cuda::DescriptorMatcher> gpuMatcher; 
+
+    };
+
 
     class Frame
     {
     public: 
-        
+        Eigen::Matrix3d K_eigen, invK_eigen; 
+        Eigen::Matrix4d pose_eigen;
+        std::vector<cv::KeyPoint> frameFeaturePoints;  
+        cv::Mat descriptors; 
+
+        Frame(const cv::Mat &img, )
     private: 
 
-
-    }
+    };
 }
