9eb52b5a2dcb7a045564950f623eba1f7c06b434
DizkiBorsuk <maciekrostocki@gmail.com>
2023-07-23 10:15:59 +0200

    added issue, have to finish frame graber

--- visual_slam_cpp/src/FrameExtraction.cpp
+++ visual_slam_cpp/src/FrameExtraction.cpp
@@ -58,6 +58,7 @@ namespace mrVSLAM
         detector->detect(img, keypoints); 
         descriptor->compute(img, keypoints, descriptors); 
 
+        std::cout << "descriptors size" << descriptors.size()<< "\n"; 
         return {keypoints, descriptors}; 
     }    
     
@@ -67,76 +68,73 @@ namespace mrVSLAM
         //cv::Ptr<cv::cuda::FastFeatureDetector> gpuFastDetector; 
         cv::Ptr<cv::cuda::ORB> gpuOrbExtractor = cv::cuda::ORB::create(500,1.200000048F, 8, 31, 0, 2, 0, 31, 20, true);
         gpuOrbExtractor->detectAndComputeAsync(img,cv::noArray(), keypoints, descriptors, false); 
-        // cv::Ptr<cv::cuda::ORB> gpu_detector = cv::cuda::ORB::create(num_features,1.200000048F, 8, 31, 0, 2, 0, 31, 20, true);  
         // //gpu_ORB->detectAndComputeAsync(frame, cv::noArray(), gpu_keypoints_1, gpu_descriptors_1, false); 
-        // //gpu_detector->detectAndCompute(frame, cv::noArray(), keypoints_1, gpu_descriptors_1, false); 
         
         // gpu_detector->detect(frame, keypoints_1);
-        // std::cout << "Number of features: " << keypoints_1.size() << "\n"; 
         // //gpu_ORB->compute(frame, gpu_keypoints_1, descriptors_1_gpu); 
-        // //gpu_ORB->convert(gpu_keypoints_1, keypoints_1); 
+
         return {keypoints, descriptors}; 
     }
 
-    void FrameMatcher::matchFeaturesFlann(const float& low_rt) noexcept
-    {    
-        matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED); 
-        descriptors.convertTo(descriptors, CV_32F);
+    // void FrameMatcher::matchFeaturesFlann(const float& low_rt) noexcept
+    // {    
+    //     matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED); 
+    //     descriptors.convertTo(descriptors, CV_32F);
 
-        std::vector<std::vector<cv::DMatch>> matches;
-        cv::Point2f keypoint1, keypoint2; 
-        std::vector<cv::Point2f> point_pair;  
+    //     std::vector<std::vector<cv::DMatch>> matches;
+    //     cv::Point2f keypoint1, keypoint2; 
+    //     std::vector<cv::Point2f> point_pair;  
 
-        if(!prev_descriptors.empty())
-        {
-            matcher->knnMatch(descriptors, prev_descriptors, matches, 2); // finds 2 best matches for descriptors 
+    //     if(!prev_descriptors.empty())
+    //     {
+    //         matcher->knnMatch(descriptors, prev_descriptors, matches, 2); // finds 2 best matches for descriptors 
 
-            for (size_t i = 0; i < matches.size(); i++)
-            {
-                if (matches[i][0].distance < low_rt * matches[i][1].distance) // compare 1 best match with second best match  
-                {
-                    good_matches.emplace_back(matches[i][0]);
-                    keypoint1 = frame_keypoints[matches[i][0].queryIdx].pt;  
-                    keypoint2 = prev_keyPs[matches[i][0].trainIdx].pt; 
-                    point_pair.emplace_back(keypoint1); 
-                    point_pair.emplace_back(keypoint2); 
-                    matched_keypoints.emplace_back(point_pair); 
-                }
-                point_pair.clear(); 
-            }
-        }
-        prev_descriptors = descriptors; 
-        prev_keyPs = frame_keypoints; 
-    }
+    //         for (size_t i = 0; i < matches.size(); i++)
+    //         {
+    //             if (matches[i][0].distance < low_rt * matches[i][1].distance) // compare 1 best match with second best match  
+    //             {
+    //                 good_matches.emplace_back(matches[i][0]);
+    //                 keypoint1 = frame_keypoints[matches[i][0].queryIdx].pt;  
+    //                 keypoint2 = prev_keyPs[matches[i][0].trainIdx].pt; 
+    //                 point_pair.emplace_back(keypoint1); 
+    //                 point_pair.emplace_back(keypoint2); 
+    //                 matched_keypoints.emplace_back(point_pair); 
+    //             }
+    //             point_pair.clear(); 
+    //         }
+    //     }
+    //     prev_descriptors = descriptors; 
+    //     prev_keyPs = frame_keypoints; 
+    // }
 
-    void FrameMatcher::matchFeaturesBF(const float& low_rt) noexcept
-    {    
-        matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::BRUTEFORCE_HAMMING); 
+    // void FrameMatcher::matchFeaturesBF(const float& low_rt) noexcept
+    // {    
+    //     matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::BRUTEFORCE_HAMMING); 
 
-        std::vector<std::vector<cv::DMatch>> matches;
-        cv::Point2f keypoint1, keypoint2; 
-        std::vector<cv::Point2f> point_pair; 
+    //     std::vector<std::vector<cv::DMatch>> matches;
+    //     cv::Point2f keypoint1, keypoint2; 
+    //     std::vector<cv::Point2f> point_pair; 
 
-        if(!prev_descriptors.empty())
-        {
-            matcher->knnMatch(descriptors, prev_descriptors, matches, 2); 
+    //     if(!prev_descriptors.empty())
+    //     {
+    //         matcher->knnMatch(descriptors, prev_descriptors, matches, 2); 
 
-            for (size_t i = 0; i < matches.size(); i++)
-            {
-                if (matches[i][0].distance < low_rt * matches[i][1].distance)
-                {
-                    good_matches.emplace_back(matches[i][0]);
-                    keypoint1 = frame_keypoints[matches[i][0].queryIdx].pt;  
-                    keypoint2 = prev_keyPs[matches[i][0].trainIdx].pt; 
-                    point_pair.emplace_back(keypoint1); 
-                    point_pair.emplace_back(keypoint2); 
-                    matched_keypoints.emplace_back(point_pair); 
+    //         for (size_t i = 0; i < matches.size(); i++)
+    //         {
+    //             if (matches[i][0].distance < low_rt * matches[i][1].distance)
+    //             {
+    //                 good_matches.emplace_back(matches[i][0]);
+    //                 keypoint1 = frame_keypoints[matches[i][0].queryIdx].pt;  
+    //                 keypoint2 = prev_keyPs[matches[i][0].trainIdx].pt; 
+    //                 point_pair.emplace_back(keypoint1); 
+    //                 point_pair.emplace_back(keypoint2); 
+    //                 matched_keypoints.emplace_back(point_pair); 
 
-                }
-            point_pair.clear(); 
-            }
-        }
-        prev_descriptors = descriptors; 
-        prev_keyPs = frame_keypoints; 
-    }
+    //             }
+    //         point_pair.clear(); 
+    //         }
+    //     }
+    //     prev_descriptors = descriptors; 
+    //     prev_keyPs = frame_keypoints; 
+    // }
 }
