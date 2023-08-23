49847982f88c28d7bbf60b311b924de2e4b909a1
DizkiBorsuk <maciekrostocki@gmail.com>
2023-08-23 12:37:32 +0200

    some work on functions in map and mappoint

--- visual_slam_cpp/temp_slam/include/map.hpp
+++ visual_slam_cpp/temp_slam/include/map.hpp
@@ -16,9 +16,9 @@ namespace mrVSLAM
     public: 
         //### data members ###// 
         unsigned int  id = 0; // map point id 
-        unsigned int frames_in = 0; // how many times/frames point was observed
+        unsigned int frames_in = 0; // how many times/frames point was observed during matching 
         std::array<float, 3> position {}; // position of point in X,Y,Z 
-        // maybe std::list instead of vector? // bjourne says noo 
+        //? maybe std::list instead of vector? // bjourne says noo 
         std::vector<std::weak_ptr<Feature>> point_features; // features that describe point, set of features in which point was seen 
 
         std::mutex point_mutex; 
@@ -27,7 +27,7 @@ namespace mrVSLAM
         MapPoint(unsigned int id, std::array<float,3> point_position) noexcept; 
 
         std::array<float, 3> getPosition(); 
-        void SetPosition(const std::array<float, 3> &point_position); 
+        void setPosition(const std::array<float, 3> &point_position); 
 
         void addFeature(std::shared_ptr<Feature> feature); // add feature in wich point was observed, can be more that one 
         void removeFeature(std::shared_ptr<Feature> feature); 
@@ -46,20 +46,20 @@ namespace mrVSLAM
         std::unordered_map<unsigned int, std::shared_ptr<MapPoint>> landmarks; // map points in map 
         std::unordered_map<unsigned int, std::shared_ptr<Frame>> keyFrames; // frames in map 
         
-        std::unordered_map<unsigned int, std::shared_ptr<MapPoint>> enabled_landmarks; //maybe active? 
+        std::unordered_map<unsigned int, std::shared_ptr<MapPoint>> enabled_landmarks; //?maybe active? 
         std::unordered_map<unsigned int, std::shared_ptr<Frame>> enabled_keyframes; 
 
         std::shared_ptr<Frame> currentFrame = nullptr; 
         unsigned int num_of_enabled_keyframes = 5; 
 
         //### function members ###// 
-        Map() noexcept {};
+        Map() noexcept = default;
 
-        void insertKeyFrame(std::shared_ptr<Frame> frame);
-        void insertPoint(std::shared_ptr<MapPoint> point);   
+        void insertKeyFrame(std::shared_ptr<Frame> keyframe);
+        void insertPoint(std::shared_ptr<MapPoint> mappoint);   
 
-        void CleanMap(); 
-        void removeKeyFrame(); // function for removing old/unnecessary keyframes 
+        void cleanMap(); 
+        void removeOldestKeyFrame(); // function for removing old/unnecessary keyframes 
 
     private: 
 
--- visual_slam_cpp/temp_slam/src/map.cpp
+++ visual_slam_cpp/temp_slam/src/map.cpp
@@ -7,4 +7,82 @@ namespace mrVSLAM
     {
 
     }
+
+    std::array<float,3> MapPoint::getPosition()
+    {
+        std::lock_guard<std::mutex> lock(point_mutex); 
+        return position; 
+    }
+
+    void MapPoint::setPosition(const std::array<float, 3> &point_position)
+    {
+        std::lock_guard<std::mutex> lock(point_mutex); 
+        position = point_position; 
+    }
+
+    void MapPoint::addFeature(std::shared_ptr<Feature> feature)
+    {
+        // andd Feature object to point 
+        std::lock_guard<std::mutex> lock(point_mutex); 
+        point_features.emplace_back(feature); 
+        frames_in++; 
+    }
+
+    void MapPoint::removeFeature(std::shared_ptr<Feature> feature)
+    {
+        std::lock_guard<std::mutex> lock(point_mutex); 
+
+    }
+
+    // ########## // 
+
+    void Map::insertKeyFrame(std::shared_ptr<Frame> keyframe)
+    {
+        currentFrame = keyframe; 
+        if(keyFrames.contains(keyframe->keyframe_id)) // check if hashmap already contains said frame with that id
+        {
+            //if hashmap already contains frame with that id replace pointer to it? 
+            //? not sure about that 
+            
+
+        } 
+        else 
+        {
+            //if not, put it in keyframes map 
+            keyFrames.insert({keyframe->keyframe_id, keyframe}); 
+            enabled_keyframes.insert({keyframe->keyframe_id, keyframe});
+        }
+
+        if(enabled_keyframes.size() > num_of_enabled_keyframes)
+        {
+            removeOldestKeyFrame(); 
+        }
+    }
+
+    void Map::insertPoint(std::shared_ptr<MapPoint> mappoint)
+    {
+        if(landmarks.contains(mappoint->id))
+        {
+            //? 
+            landmarks[mappoint->id] = mappoint; 
+            enabled_landmarks[mappoint->id] = mappoint
+        }
+        else 
+        {
+            landmarks.insert({mappoint->id, mappoint});
+            enabled_landmarks.insert({mappoint->id, mappoint});
+        }
+    }
+
+    void Map::cleanMap()
+    {
+
+    }
+
+    void Map::removeOldestKeyFrame()
+    {
+
+    }
+
+
 }
