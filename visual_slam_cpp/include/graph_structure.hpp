d55f6deff6afea789228fe9085bad4597f3c370f
DizkiBorsuk <maciekrostocki@gmail.com>
2023-10-09 00:31:50 +0200

    found problem

--- visual_slam_cpp/include/graph_structure.hpp
+++ visual_slam_cpp/include/graph_structure.hpp
@@ -16,16 +16,16 @@
 #include "g2o/types/sba/types_six_dof_expmap.h"
 
 
-g2o::SE3Quat eigenToSE3quat(const Eigen::Matrix4d &T)
-{
-   /* Convertion from Homogenous Transformation Matrix in Eigen to g2o Quaterion, because g2o is stupid */
-   Eigen::Matrix3d R; // Rotation matrix 
-   Eigen::Vector3d t; //translation vector 
-   R = T.block<3,3>(0,0); 
-   t = T.col(3); 
-   //not sure if it works //!add tests 
-   return g2o::SE3Quat(R, t); //http://docs.ros.org/en/fuerte/api/re_vision/html/classg2o_1_1SE3Quat.html 
-}
+// g2o::SE3Quat eigenToSE3quat(const Eigen::Matrix4d &T)
+// {
+//    /* Convertion from Homogenous Transformation Matrix in Eigen to g2o Quaterion, because g2o is stupid */
+//    Eigen::Matrix3d R; // Rotation matrix 
+//    Eigen::Vector3d t; //translation vector 
+//    R = T.block<3,3>(0,0); 
+//    t = T.col(3); 
+//    //not sure if it works //!add tests 
+//    return g2o::SE3Quat(R, t); //http://docs.ros.org/en/fuerte/api/re_vision/html/classg2o_1_1SE3Quat.html 
+// }
 
 
 class Pose3DVertex : public g2o::BaseVertex<6, Sophus::SE3d> // class Pose3DVertex : public g2o::BaseVertex<6, Eigen::Matrix4d>
@@ -101,7 +101,7 @@ public:
    {
       const Pose3DVertex* vertex = static_cast<Pose3DVertex*>(_vertices[0]); //_verticies - g2o variable
       Sophus::SE3d T = vertex->estimate(); 
-      Eigen::Vector3d homogenous_pixel_pos = (T*position_in_3D)*K; // do point projection to img // 
+      Eigen::Vector3d homogenous_pixel_pos = K*(T*position_in_3D); // do point projection to img // 
       homogenous_pixel_pos = homogenous_pixel_pos/homogenous_pixel_pos[2]; // convert to non homogenous by dividing by w/scale factor
       Eigen::Vector2d pixel_position = homogenous_pixel_pos.head<2>(); 
       _error = _measurement - pixel_position; // calculate error of estimation 
@@ -155,7 +155,7 @@ public:
       const PointVertex* point_vertex = static_cast<PointVertex*>(_vertices[1]);
       Sophus::SE3d T = pose_vertex->estimate(); 
       
-      Eigen::Vector3d homogenous_pixel_pos = (cam_extrinsics*(T*point_vertex->estimate()))*K; 
+      Eigen::Vector3d homogenous_pixel_pos = K*(cam_extrinsics*(T*point_vertex->estimate())); 
       homogenous_pixel_pos = homogenous_pixel_pos/homogenous_pixel_pos[2]; // convert to non homogenous by dividing by w/scale factor
       Eigen::Vector2d pixel_position = homogenous_pixel_pos.head<2>(); 
       _error = _measurement - pixel_position; // calculate error of estimation 
