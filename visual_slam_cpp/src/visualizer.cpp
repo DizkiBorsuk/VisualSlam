e1b45486533661dd6fc4a8b0f0b4a296921bf857
DizkiBorsuk <maciekrostocki@gmaiol.com>
2023-10-09 20:33:18 +0200

    finally compiles, fixed mistake in vsiualizer function

--- visual_slam_cpp/src/visualizer.cpp
+++ visual_slam_cpp/src/visualizer.cpp
@@ -34,49 +34,47 @@ namespace mrVSLAM
         //* main visualizer function run by thread 
 
         pangolin::CreateWindowAndBind("slamMap_viewer",1024,768);
-        // glEnable(GL_DEPTH_TEST);
-        // glEnable(GL_BLEND);
-        // glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-        // // Define Camera Render Object (for view / scene browsing)
-        // pangolin::OpenGlRenderState s_cam(
-        //     pangolin::ProjectionMatrix(window_w,window_h,400,400,512,384,0.1,1000),
-        //     pangolin::ModelViewLookAt(0, -5, -10, 0, 0, 0, 0.0, -1.0, 0.0)
-        // );
+        // Define Camera Render Object (for view / scene browsing)
+        pangolin::OpenGlRenderState s_cam(
+            pangolin::ProjectionMatrix(window_w,window_h,400,400,512,384,0.1,1000),
+            pangolin::ModelViewLookAt(0, -5, -10, 0, 0, 0, 0.0, -1.0, 0.0));
 
-        //   // Add named OpenGL viewport to window and provide 3D Handler
-        // pangolin::View& d_cam = pangolin::CreateDisplay()
-        //     .SetBounds(0.0, 1.0, 0, 1.0, -1024.0f/768.0f)
-        //     .SetHandler(new pangolin::Handler3D(s_cam));
+          // Add named OpenGL viewport to window and provide 3D Handler
+        pangolin::View& d_cam = pangolin::CreateDisplay()
+            .SetBounds(0.0, 1.0, 0, 1.0, -1024.0f/768.0f)
+            .SetHandler(new pangolin::Handler3D(s_cam));
 
-        while(true){
-        //while( !pangolin::ShouldQuit() )
-        // {
-        //     // Clear entire screen
-        //     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);    
-        //     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        while( !pangolin::ShouldQuit() )
+        {
+            // Clear entire screen
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);    
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-        //     if(d_cam.IsShown()) {
-        //         // Activate efficiently by object
-        //         d_cam.Activate(s_cam);
-        //     }
+            if(d_cam.IsShown()) {
+                // Activate efficiently by object
+                d_cam.Activate(s_cam);
+            }
 
-        //     std::lock_guard<std::mutex> lock(visualizer_mutex); 
+            std::lock_guard<std::mutex> lock(visualizer_mutex); 
 
-        //     if(current_frame != nullptr)
-        //     {
-        //         drawFrame(current_frame, green); 
-        //         drawFrameTrajectory(s_cam); 
+            if(current_frame != nullptr)
+            {
+                drawFrame(current_frame, green); 
+                drawFrameTrajectory(s_cam); 
 
-        //         cv::imshow("current frame", current_frame->imgLeft); 
-        //         cv::waitKey(1); 
-        //     }
+                cv::imshow("current frame", current_frame->imgLeft); 
+                cv::waitKey(1); 
+            }
 
-        //     drawPoints(red); 
+            drawPoints(red); 
 
-        //     // Swap frames and Process Events
-        //     pangolin::FinishFrame();
-        //     std::this_thread::sleep_for(5000ms); // https://en.cppreference.com/w/cpp/thread/sleep_for //? 
+            // Swap frames and Process Events
+            pangolin::FinishFrame();
+            std::this_thread::sleep_for(5000ms); // https://en.cppreference.com/w/cpp/thread/sleep_for //? 
         }
     }
 
@@ -113,7 +111,7 @@ namespace mrVSLAM
 
     void Visualizer::drawFrame(std::shared_ptr<Frame> input_frame, const float color[3])
     {   
-        Eigen::Matrix4d CameraToWorldTransformation = current_frame->framePose.inverse(); //Tcw 
+        Eigen::Matrix4d CameraToWorldTransformation = input_frame->framePose.inverse(); //Tcw 
         
         
         glPushMatrix(); 
